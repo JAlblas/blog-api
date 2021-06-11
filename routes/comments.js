fbe9ee49299e1b4a092d0aa16597cdf57b86b0f7
Jasper Alblas <just_jasper@hotmail.com>
2021-06-11 12:32:11 +0200

    adds user routes

--- routes/comments.js
+++ routes/comments.js
@@ -3,27 +3,13 @@ var router = express.Router();
 
 var commentController = require('../controllers/commentController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json("No comments");
-});
+router.post('/', commentController.createComment);
 
-router.post('/', function(req, res, next) {
-    res.json("Creating comment");
-  });
+router.get('/', postController.getPosts);
+router.get('/:id', postController.getPost);
 
-router.get('/:id', function(req, res, next) {
-    res.json(req.params.id);
-  });
+router.put('/:id', postController.updatePost);
 
-router.put('/:id', function(req, res, next) {
-// Update
-res.json(req.params.id);
-});
-
-router.delete('/:id', function(req, res, next) {
-    // Delete
-res.json(req.params.id);
-});
+router.delete('/:id', postController.deletePost);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,12 @@ var router = express.Router();
 
 var userController = require('../controllers/userController');
 
-router.get('/', userController.getUsers);
 router.post('/', userController.createUser);
+
+router.get('/', userController.getUsers);
 router.get('/:id', userController.getUser);
 
-router.put('/:id', function(req, res, next) {
-  // Update
-  res.json(req.params.id);
-});
+router.put('/:id', userController.updateUser);
 
-router.delete('/:id', function(req, res, next) {
-    // Delete
-  res.json(req.params.id);
-});
-
+router.delete('/:id', userController.deleteUser);
 module.exports = router;
